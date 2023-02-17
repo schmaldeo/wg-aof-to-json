4a2e79c3a6b0cf6a6c7bf143a38f05c59a375fa7
schmatteo <michal.skrzyszowski@hotmail.com>
2023-02-17 11:11:35 +0100

    Final refactor

--- players.js
+++ players.js
@@ -13,7 +13,7 @@ const fetchPlayers = async () => {
     // the fastest way to fetch the data from API is to use Promise.all,
     // however Wargaming API will not let you do it because of DDOS protection,
     // therefore you need to limit the amount of requests for the API not to return corrupted data
-    const limit = pLimit(8);
+    const limit = pLimit(10);
     // get the amount of pages
     const pageRes = await fetch(`https://api.worldoftanks.eu/wot/globalmap/eventaccountratings/?application_id=${appId}&event_id=${eventId}&front_id=${frontId}&fields=rank&limit=100&in_rating=1`).then((e) => e.json());
     const pageCount = pageRes.meta.page_total;
@@ -24,54 +24,51 @@ const fetchPlayers = async () => {
         urls.push(url);
     }
     // fetch data
-    const initDataTBP = urls.map((e) => limit(() => fetch(e).then((r) => r.json())));
-    const fetchData = await Promise.all(initDataTBP);
+    const fetchDataPromises = urls.map((e) => limit(() => fetch(e).then((r) => r.json())));
+    const fetchData = await Promise.all(fetchDataPromises);
     // put the actual data into an array, ignoring metadata
     const data = [];
-    // for (const page of fetchData) {
-    //   data.push(page.data);
-    // }
     fetchData.forEach((page) => {
         data.push(page.data);
     });
     // making arrays of URLs to fetch nicknames based on account id and clan that account is in
     // (API doesnt provide either)
-    const pUrls = [];
-    const cUrls = [];
+    const playersUrls = [];
+    const clanUrls = [];
     data.forEach((page) => {
-        const iterationOfPIDs = [];
-        const iterationOfCIDs = [];
-        page && page.forEach((player) => {
-            iterationOfPIDs.push(player.account_id);
-            if (iterationOfCIDs.indexOf(player.clan_id) === -1 && player.clan_id) {
-                iterationOfCIDs.push(player.clan_id);
+        const playerIDIteration = [];
+        const clanIDIteration = [];
+        page.forEach((player) => {
+            playerIDIteration.push(player.account_id);
+            if (clanIDIteration.indexOf(player.clan_id) === -1 && player.clan_id) {
+                clanIDIteration.push(player.clan_id);
             }
         });
-        pUrls.push(`https://api.worldoftanks.eu/wot/account/info/?application_id=${appId}&account_id=${iterationOfPIDs}&fields=nickname`);
-        cUrls.push(`https://api.worldoftanks.eu/wot/clans/info/?application_id=${appId}&fields=tag%2C+name&clan_id=${iterationOfCIDs}`);
+        playersUrls.push(`https://api.worldoftanks.eu/wot/account/info/?application_id=${appId}&account_id=${playerIDIteration}&fields=nickname`);
+        clanUrls.push(`https://api.worldoftanks.eu/wot/clans/info/?application_id=${appId}&fields=tag%2C+name&clan_id=${clanIDIteration}`);
     });
     // fetching nicknames and clans
-    const initPlayersData = pUrls.map((e) => limit(() => fetch(e).then((r) => r.json())));
-    const initClansData = cUrls.map((e) => limit(() => fetch(e).then((r) => r.json())));
-    const playersData = await Promise.all(initPlayersData);
-    const clansData = await Promise.all(initClansData);
+    const playersDataPromises = playersUrls.map((e) => limit(() => fetch(e).then((r) => r.json())));
+    const clansDataPromises = clanUrls.map((e) => limit(() => fetch(e).then((r) => r.json())));
+    const playersData = await Promise.all(playersDataPromises);
+    const clansData = await Promise.all(clansDataPromises);
     data.forEach((page) => {
         page.forEach((player) => {
             if (player.account_id) {
                 const accId = player.account_id.toString();
-                for (const e of playersData) {
-                    if (!e.data[accId])
+                for (const playerRes of playersData) {
+                    if (!playerRes.data[accId])
                         continue;
-                    player.nickname = e.data[accId].nickname;
+                    player.nickname = playerRes.data[accId].nickname;
                 }
             }
             if (player.clan_id) {
                 const clanId = player.clan_id.toString();
-                for (const e of clansData) {
-                    if (!e.data[clanId])
+                for (const clanRes of clansData) {
+                    if (!clanRes.data[clanId])
                         continue;
-                    player.clanname = e.data[clanId].name;
-                    player.tag = e.data[clanId].tag;
+                    player.clanname = clanRes.data[clanId].name;
+                    player.tag = clanRes.data[clanId].tag;
                 }
             }
         });
--- players.ts
+++ players.ts
@@ -61,7 +61,7 @@ const fetchPlayers = async () => {
   // the fastest way to fetch the data from API is to use Promise.all,
   // however Wargaming API will not let you do it because of DDOS protection,
   // therefore you need to limit the amount of requests for the API not to return corrupted data
-  const limit = pLimit(8);
+  const limit = pLimit(10);
 
   // get the amount of pages
   const pageRes = await fetch(`https://api.worldoftanks.eu/wot/globalmap/eventaccountratings/?application_id=${appId}&event_id=${eventId}&front_id=${frontId}&fields=rank&limit=100&in_rating=1`).then((e) => e.json()) as ApiResponse;
@@ -75,14 +75,11 @@ const fetchPlayers = async () => {
   }
 
   // fetch data
-  const initDataTBP = urls.map((e) => limit(() => fetch(e).then((r) => r.json())));
-  const fetchData = await Promise.all(initDataTBP) as ApiResponse[];
+  const fetchDataPromises = urls.map((e) => limit(() => fetch(e).then((r) => r.json())));
+  const fetchData = await Promise.all(fetchDataPromises) as ApiResponse[];
 
   // put the actual data into an array, ignoring metadata
   const data: Array<Player[]> = [];
-  // for (const page of fetchData) {
-  //   data.push(page.data);
-  // }
 
   fetchData.forEach((page) => {
     data.push(page.data);
@@ -90,43 +87,43 @@ const fetchPlayers = async () => {
 
   // making arrays of URLs to fetch nicknames based on account id and clan that account is in
   // (API doesnt provide either)
-  const pUrls: string[] = [];
-  const cUrls: string[] = [];
+  const playersUrls: string[] = [];
+  const clanUrls: string[] = [];
   data.forEach((page) => {
-    const iterationOfPIDs: number[] = [];
-    const iterationOfCIDs: number[] = [];
-    page && page.forEach((player) => {
-      iterationOfPIDs.push(player.account_id);
-      if (iterationOfCIDs.indexOf(player.clan_id) === -1 && player.clan_id) {
-        iterationOfCIDs.push(player.clan_id);
+    const playerIDIteration: number[] = [];
+    const clanIDIteration: number[] = [];
+    page.forEach((player) => {
+      playerIDIteration.push(player.account_id);
+      if (clanIDIteration.indexOf(player.clan_id) === -1 && player.clan_id) {
+        clanIDIteration.push(player.clan_id);
       }
     });
-    pUrls.push(`https://api.worldoftanks.eu/wot/account/info/?application_id=${appId}&account_id=${iterationOfPIDs}&fields=nickname`);
-    cUrls.push(`https://api.worldoftanks.eu/wot/clans/info/?application_id=${appId}&fields=tag%2C+name&clan_id=${iterationOfCIDs}`);
+    playersUrls.push(`https://api.worldoftanks.eu/wot/account/info/?application_id=${appId}&account_id=${playerIDIteration}&fields=nickname`);
+    clanUrls.push(`https://api.worldoftanks.eu/wot/clans/info/?application_id=${appId}&fields=tag%2C+name&clan_id=${clanIDIteration}`);
   });
 
   // fetching nicknames and clans
-  const initPlayersData = pUrls.map((e) => limit(() => fetch(e).then((r) => r.json())));
-  const initClansData = cUrls.map((e) => limit(() => fetch(e).then((r) => r.json())));
+  const playersDataPromises = playersUrls.map((e) => limit(() => fetch(e).then((r) => r.json())));
+  const clansDataPromises = clanUrls.map((e) => limit(() => fetch(e).then((r) => r.json())));
 
-  const playersData = await Promise.all(initPlayersData) as PlayerApiResponse[];
-  const clansData = await Promise.all(initClansData) as ClanApiResponse[];
+  const playersData = await Promise.all(playersDataPromises) as PlayerApiResponse[];
+  const clansData = await Promise.all(clansDataPromises) as ClanApiResponse[];
 
   data.forEach((page) => {
     page.forEach((player) => {
       if (player.account_id) {
         const accId = player.account_id.toString();
-        for (const e of playersData) {
-          if (!e.data[accId]) continue;
-          player.nickname = e.data[accId].nickname;
+        for (const playerRes of playersData) {
+          if (!playerRes.data[accId]) continue;
+          player.nickname = playerRes.data[accId].nickname;
         }
       }
       if (player.clan_id) {
         const clanId = player.clan_id.toString();
-        for (const e of clansData) {
-          if (!e.data[clanId]) continue;
-          player.clanname = e.data[clanId].name;
-          player.tag = e.data[clanId].tag;
+        for (const clanRes of clansData) {
+          if (!clanRes.data[clanId]) continue;
+          player.clanname = clanRes.data[clanId].name;
+          player.tag = clanRes.data[clanId].tag;
         }
       }
     });
